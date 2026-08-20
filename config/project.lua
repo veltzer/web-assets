0ccc584ae8a36fce810d418f67cea2717e583866
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-21 01:48:11 +0300

    no commit message given

---
+++ config/project.lua
@@ -0,0 +1,3 @@
+NAME = "assets"
+DESCRIPTION_SHORT = "Images, videos and other assets to be used by other projects"
+KEYWORDS = {"mermaid", "marp", "jpg"}
--- config/project.py
+++
@@ -1,8 +0,0 @@
-""" project definitions """
-name = "assets"
-description_short = "Images, vidoes and other assets to be used by other projects"
-keywords = [
-    "mermaid",
-    "marp",
-    "jpg",
-]
